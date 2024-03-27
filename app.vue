2c229ccbfae3bc4f8eb4083e7d77e38352aef552
luizhf42 <luizhf42@gmail.com>
2024-03-27 17:23:57 -0300

    feat: separate form component

--- app.vue
+++ app.vue
@@ -1,30 +1,9 @@
 <template>
-	<form>
-		<SexField />
-		<div>
-			<BodyMetricInput metric="Age" suffix="yrs" />
-			<BodyMetricInput metric="Weight" suffix="kg" />
-			<BodyMetricInput metric="Height" suffix="cm" />
-			<BodyMetricInput metric="Neck" suffix="cm" />
-			<BodyMetricInput metric="Waist" suffix="cm" />
-			<BodyMetricInput metric="Hip" suffix="cm" />
-		</div>
-		<button>Calculate</button>
-	</form>
+	<Form />
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped lang="postcss">
-form {
-	@apply bg-white rounded-lg mx-auto border shadow w-11/12 max-w-lg grid gap-3 p-6;
 
-	div {
-		@apply flex flex-col xs:flex-row flex-wrap justify-between;
-	}
-
-	button {
-		@apply bg-blue-500 text-white font-semibold py-2 px-4 rounded-md;
-	}
-}
 </style>
