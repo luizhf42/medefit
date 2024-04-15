bbb797e761e0cda591ca9cf05b93f3f9542ba813
luizhf42 <luizhf42@gmail.com>
2024-04-15 17:02:51 -0300

    feat: add result component scratch

---
+++ components/Result.vue
@@ -0,0 +1,13 @@
+<template>
+	<p>sua gordura corporal é {{ bodyFat }}</p>
+	<p>seu IMC é {{ bmi }}</p>
+</template>
+
+<script setup lang="ts">
+const { bmi, bodyFat } = defineProps<{
+	bmi: number;
+	bodyFat: number;
+}>();
+</script>
+
+<style scoped lang="postcss"></style>
